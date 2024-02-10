297fa986c1cd283ddb232073bf8ecb19bca324c6
Carlos Dias <eduardovinagre123@gmail.com>
2024-02-10 12:18:19 -0300

    feat(opencvimageprocessor): inject ImageDataAdapter in OpenCVImageProcessor for handling conversions

--- src/data/OpenCVImageProcessor.ts
+++ src/data/OpenCVImageProcessor.ts
@@ -2,17 +2,17 @@ import * as OpenCV from "@u4/opencv4nodejs";
 
 class OpenCVImageProcessor implements ImageProcessor {
     protected readonly openCV: typeof OpenCV;
-    protected readonly imageProcessorAdapter: ImageProcessorAdapter;
+    protected readonly imageDataAdapter: ImageDataAdapter;
 
-    constructor(openCV: typeof OpenCV, imageProcessorAdapter: ImageProcessorAdapter) {
+    constructor(openCV: typeof OpenCV, imageDataAdapter: ImageDataAdapter) {
         this.openCV = openCV;
-        this.imageProcessorAdapter = imageProcessorAdapter;
+        this.imageDataAdapter = imageDataAdapter;
     }
 
     loadImageFromSource(src: string): ImageMatrix {
         try {
             const openCVImage = this.openCV.imread(src);
-            return this.imageProcessorAdapter.convertOriginalDataToImageMatrix(openCVImage)
+            return this.imageDataAdapter.convertOriginalDataToImageMatrix(openCVImage)
         }
         catch (err) {
             throw new Error(err.message);
@@ -20,34 +20,34 @@ class OpenCVImageProcessor implements ImageProcessor {
     };
 
     convertToGrayscale(currentImage: ImageMatrix): ImageMatrix {
-        const openCVConvertedCurrentImage = this.imageProcessorAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
+        const openCVConvertedCurrentImage = this.imageDataAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
         const openCVGrayscaledImage = openCVConvertedCurrentImage.bgrToGray();
-        return this.imageProcessorAdapter.convertOriginalDataToImageMatrix(openCVGrayscaledImage);
+        return this.imageDataAdapter.convertOriginalDataToImageMatrix(openCVGrayscaledImage);
     }
 
     applyBinaryThreshold(currentImage: ImageMatrix, { thresholdNumber, maximumValue, type }: BinaryThresholdParameters): ImageMatrix {
-        const openCVConvertedCurrentImage = this.imageProcessorAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
+        const openCVConvertedCurrentImage = this.imageDataAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
         const thresholdedImage = openCVConvertedCurrentImage.threshold(thresholdNumber, maximumValue, type)
-        return this.imageProcessorAdapter.convertOriginalDataToImageMatrix(thresholdedImage)
+        return this.imageDataAdapter.convertOriginalDataToImageMatrix(thresholdedImage)
     }
 
     getRegionOfInterest(currentImage: ImageMatrix, { x, y, width, height }: Rectangle): ImageMatrix {
-        const openCVConvertedCurrentImage = this.imageProcessorAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
+        const openCVConvertedCurrentImage = this.imageDataAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
 
         const openCVRectangle = new this.openCV.Rect(x, y, width, height);
 
         const ROI = openCVConvertedCurrentImage.getRegion(openCVRectangle);
 
-        return this.imageProcessorAdapter.convertOriginalDataToImageMatrix(ROI)
+        return this.imageDataAdapter.convertOriginalDataToImageMatrix(ROI)
     }
 
     rotateImageByDegrees(currentImage: ImageMatrix, { centerPoint, angle }: RotateImageParameters) {
         const openCVPoint2D = new this.openCV.Point2(centerPoint.x, centerPoint.y);
         /* TODO: Write util function to receive sorted angles array and then rotate it from its array median angle*/
-        const openCVConvertedCurrentImage = this.imageProcessorAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
+        const openCVConvertedCurrentImage = this.imageDataAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
         const rotationMatrix = this.openCV.getRotationMatrix2D(openCVPoint2D, angle);
         const rotatedImage = openCVConvertedCurrentImage.warpAffine(rotationMatrix)
-        return this.imageProcessorAdapter.convertOriginalDataToImageMatrix(rotatedImage)
+        return this.imageDataAdapter.convertOriginalDataToImageMatrix(rotatedImage)
     }
 
     getImageCenterPoint(currentImage: ImageMatrix): Point2D {
@@ -55,7 +55,7 @@ class OpenCVImageProcessor implements ImageProcessor {
     }
 
     getRectanglesAngularOrientation(currentImage: ImageMatrix): number[] {
-        const openCVConvertedCurrentImage = this.imageProcessorAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
+        const openCVConvertedCurrentImage = this.imageDataAdapter.convertImageMatrixToOriginalData(currentImage) as OpenCV.Mat;
         openCVConvertedCurrentImage.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
         const cornerIndicatorsRotatedRectangles = cornerIndicatorsCountours.map((currentContour) => currentContour.minAreaRect())
         const rectanglesAngles = cornerIndicatorsRotatedRectangles.map(({ angle, size }) => {
